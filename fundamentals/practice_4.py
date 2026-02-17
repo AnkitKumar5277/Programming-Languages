8f6b36b8201f6bb76851605fcc2215d2041e8dd0
Ankit Kumar <121275934+AnkitKumar5277@users.noreply.github.com>
2026-02-18 00:10:50 +0530

    Update practice_4.py

--- fundamentals/practice_4.py
+++ fundamentals/practice_4.py
@@ -43,4 +43,22 @@ result = [[X[j][i] for j in range(len(X))] for i in range(len(X[0]))]
 for r in result:
    print(r)
 
+# Python Program to Find Numbers Divisible by Another Number
+my_list = [12, 65, 54, 39, 102, 339, 221,]
+# use anonymous function to filter
+result = list(filter(lambda x: (x % 13 == 0), my_list))
+print("Numbers divisible by 13 are",result)
+# Numbers divisible by 13 are [65, 39, 221]
+
+# Write a function to check if a number is divisible by five.
+# Return True if the number is divisible by 5. Otherwise, return False.
+def is_divisible_by_five(number):
+    return number % 5 == 0
+# Example usage
+print(is_divisible_by_five(10))  # True
+print(is_divisible_by_five(12))  # False
+print(is_divisible_by_five(0))   # True
+print(is_divisible_by_five(-15)) # True
+
+
 
