89e73250306e7891cd1e28bb036df5ac93ba1067
Ankit Kumar <121275934+AnkitKumar5277@users.noreply.github.com>
2026-02-17 23:47:20 +0530

    Update practice_4.py

--- fundamentals/practice_4.py
+++ fundamentals/practice_4.py
@@ -21,3 +21,26 @@ number = 5
 result = sum_of_natural_numbers(number)
 print(f"The sum of the first {number} natural numbers is {result}.")
 
+# Program to transpose a matrix using a nested loop
+X = [[12,7],
+    [4 ,5],
+    [3 ,8]]
+result = [[0,0,0],
+         [0,0,0]]
+for i in range(len(X)):
+   for j in range(len(X[0])):
+       result[j][i] = X[i][j]
+for r in result:
+   print(r)
+# [12, 4, 3]
+# [7, 5, 8]
+
+''' Program to transpose a matrix using list comprehension'''
+X = [[12,7],
+    [4 ,5],
+    [3 ,8]]
+result = [[X[j][i] for j in range(len(X))] for i in range(len(X[0]))]
+for r in result:
+   print(r)
+
+
