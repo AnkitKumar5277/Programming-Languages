618619433dbea81c9988322738c66f3433735815
Ankit Kumar <121275934+AnkitKumar5277@users.noreply.github.com>
2026-02-28 11:55:09 +0530

    Update strong.py

--- Advance code/Python Test/strong.py
+++ Advance code/Python Test/strong.py
@@ -196,5 +196,17 @@ def outer_function():
 outer_function()
 # inner_function()
 
+def add_before_ui_after_ui(func):
+     def wrapper():
+         print("Before the running UI TC")
+         print("Start the Browser ")
+         func()
+         print("Ending the running UI TC")
+         print("Quit the Browser!")
+     return wrapper()
+
+@add_before_ui_after_ui
+def test_ui():
+     print(">> I will Test the UI")
 
 
