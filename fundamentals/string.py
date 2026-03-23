40be4536cbd361ad7986a94069d259838d40a777
Ankit Kumar <121275934+AnkitKumar5277@users.noreply.github.com>
2026-03-24 00:38:56 +0530

    Update string.py

--- fundamentals/string.py
+++ fundamentals/string.py
@@ -37,3 +37,11 @@ print(capitalized)  # Output: Hello
 text = "hello"
 capitalized = text.capitalize()
 print(capitalized)  # Output: Hello
+
+For Example:
+demo_string='ROSES'
+print(demo_string.lower())
+
+text = "Hello World"
+lower_text = text.lower()
+print(lower_text)  # Output: hello world
