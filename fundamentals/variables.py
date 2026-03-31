303b184f0a64f9793a50ab1a530551cf8f22769b
Ankit Kumar <121275934+AnkitKumar5277@users.noreply.github.com>
2026-04-01 00:15:30 +0530

    Update variables.py

--- fundamentals/variables.py
+++ fundamentals/variables.py
@@ -4,3 +4,18 @@ print(a is b)  # Output: True (because small integers share the same memory in P
 
 name = "Hello"
 print("H" in name)  # Output: True
+
+# Find square root of real or complex numbers
+import cmath
+num = 1+2j
+num = eval(input('Enter a number: '))
+num_sqrt = cmath.sqrt(num)
+print('The square root of {0} is {1:0.3f}+{2:0.3f}j'.format(num ,num_sqrt.real,num_sqrt.imag))
+
+# Python Program to find the area of triangle
+a = float(input('Enter first side: '))
+b = float(input('Enter second side: '))
+c = float(input('Enter third side: '))
+s = (a + b + c) / 2
+area = (s*(s-a)*(s-b)*(s-c)) ** 0.5
+print('The area of the triangle is %0.2f' %area)
