39c8551d9adb72a9a123e2df31abbbfab6af4c50
Ankit Kumar <121275934+AnkitKumar5277@users.noreply.github.com>
2026-04-02 10:51:22 +0530

    Update function.py

--- fundamentals/function.py
+++ fundamentals/function.py
@@ -6,3 +6,12 @@ base = 3
 height = 4
 print(area_of_triangle(base, height))
 
+# Python Program to Convert Decimal to Binary Using Recursion
+def converttobinary(n):
+  if n > 1:
+    converttobinary(n//2)
+  print(n%2, end = '')
+dec = 34
+converttobinary(dec)
+print()
+# 100010
