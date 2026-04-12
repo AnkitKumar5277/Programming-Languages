2fbc688f7337a158492907827702690ad2beba17
Ankit Kumar <121275934+AnkitKumar5277@users.noreply.github.com>
2026-04-12 20:05:50 +0530

    Update list.py

--- fundamentals/list.py
+++ fundamentals/list.py
@@ -1,3 +1,20 @@
+
+# Python Program to Flatten a Nested List
+# Example 1: Using List Comprehension
+my_list = [[1],[2,3],[4,5,6,7]]
+flat_list = [num for sublist in my_list for num in sublist]
+print(flat_list)
+# [1, 2, 3, 4, 5, 6, 7]
+
+# Example 2: Using Nested for Loops (non pythonic way)
+my_list = [[1],[2,3],[4,5,6,7]]
+flat_list[]
+for sublist in my_list:
+  for num in sublist:
+    flat_list.append(num)
+print(flat_list)
+# [1, 2, 3, 4, 5, 6, 7]
+
 # Python Program to Find Numbers Divisible by Another Number
 my_list = [12, 65, 54, 39, 102, 339, 221,]
 # use anonymous function to filter
