f89d24d481e58abf2d93d4155f6692926c10e184
Ankit Kumar <121275934+AnkitKumar5277@users.noreply.github.com>
2026-04-12 20:01:59 +0530

    Update list.py

--- fundamentals/list.py
+++ fundamentals/list.py
@@ -1,3 +1,10 @@
+# Python Program to Find Numbers Divisible by Another Number
+my_list = [12, 65, 54, 39, 102, 339, 221,]
+# use anonymous function to filter
+result = list(filter(lambda x: (x % 13 == 0), my_list))
+print("Numbers divisible by 13 are",result)
+# Numbers divisible by 13 are [65, 39, 221]
+
 # list comprehension
 l = ["13", "16", "1", "5", "8"]
 l = [int(x) for x in l] 
