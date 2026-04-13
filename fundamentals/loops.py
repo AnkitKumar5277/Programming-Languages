b791bd122083ce3564c42b99216ea4cc6118ce36
Ankit Kumar <121275934+AnkitKumar5277@users.noreply.github.com>
2026-04-13 13:17:37 +0530

    Update loops.py

--- fundamentals/loops.py
+++ fundamentals/loops.py
@@ -1,4 +1,17 @@
- #           1
+# Example 5: Inverted half pyramid using numbers
+# 1 2 3 4 5
+# 1 2 3 4
+# 1 2 3
+# 1 2
+# 1
+rows = int(input("enter number of rows:"))
+for i in range(rows, 0, -1):
+  for j in range(1, i+1):
+    print(j, end=" ")
+
+  print()
+
+#           1
  #         1   1
  #       1   2   1
  #     1   3   3    1
