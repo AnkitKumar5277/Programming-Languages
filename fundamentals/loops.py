74dda1c1e6d184070423cc411c0af7703ee7b130
Ankit Kumar <121275934+AnkitKumar5277@users.noreply.github.com>
2026-04-13 13:16:41 +0530

    Update loops.py

--- fundamentals/loops.py
+++ fundamentals/loops.py
@@ -1,3 +1,19 @@
+# Example 8: Inverted full pyramid of *
+# * * * * * * * * *
+#   * * * * * * *
+#     * * * * *
+#       * * *
+#         *
+rows = int(input("Enter number of rows: "))
+for i in range(rows, 1, -1):
+    for space in range(0, rows-i):
+        print("  ", end="")
+    for j in range(i, 2*i-1):
+        print("* ", end="")
+    for j in range(1, i-1):
+        print("* ", end="")
+    print()
+
 # Programs to print full pyramids
 # Example 6: Program to print full pyramid using *
 #         *
