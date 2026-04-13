adfc2b910b82ea7556bb826dbd80100f50fd0c36
Ankit Kumar <121275934+AnkitKumar5277@users.noreply.github.com>
2026-04-14 01:12:05 +0530

    Update function.py

--- fundamentals/function.py
+++ fundamentals/function.py
@@ -1,3 +1,121 @@
+def greet():
+    print("Hello, Welcome to Python World!")
+    
+greet()
+greet()
+greet()
+
+print("dasdadads")
+greet()
+
+def _123():  # identifier rule is failed
+    print("Hi1")
+
+
+def _():
+    print("Hi")
+
+
+_()
+_123()
+
+
+def pramod123():
+    print("Hello")
+
+
+def h():
+    print("hello")
+    print("I am part of h function ?")
+
+print("Not a Part of Functions")
+
+def first_part_last_name():
+    pass  # # in future i will complete this functions
+
+
+def greet_to_all_of_you():
+    print("Hello, Everyone")
+
+
+def greet():
+    print("Yes")
+    greet_to_all_of_you()
+
+greet()
+
+def greet_by_name(name):
+    print("Hello,", name)
+    print(f"Hello, {name}")
+
+greet_by_name("Pramod")
+greet_by_name(123)
+greet_by_name(3.14)
+
+
+
+def say_hello_user(name):
+    print("Hello,", name)
+user_name = input("Enter the name : \n")
+say_hello_user(user_name)
+
+
+def print_mul_arguments(*args):
+    # *args -> List
+    for i in args:
+        print(i)
+print_mul_arguments("pramod1\n")
+print_mul_arguments("pramod", "amit", "lucky")
+print_mul_arguments("amit", 10, True, False, "PRAMOD")
+
+
+def make_pizza(*toppings):
+    print(toppings)
+    for i in toppings:
+        print(i)
+pramod = make_pizza("tomato","olives")
+jayati = make_pizza("pineapple","olives","corn","paneer")
+vinay = make_pizza("tomato")
+
+
+def max(*args):
+    for i in args:
+        print(i)
+r1 = max(1, 2, 3, 4, 6)
+r2 = max(1, 2, 3)
+r2 = max(2, 3)
+
+def outer_function():
+    var1 = 30 # local
+
+    def inner_function():
+        var2 = 90
+        print(var1)
+
+    def inner_function2():
+        print(var1)
+
+    inner_function()
+    inner_function2()
+    # print(var2)
+
+outer_function()
+# inner_function()
+
+def add_before_ui_after_ui(func):
+     def wrapper():
+         print("Before the running UI TC")
+         print("Start the Browser ")
+         func()
+         print("Ending the running UI TC")
+         print("Quit the Browser!")
+     return wrapper()
+
+@add_before_ui_after_ui
+def test_ui():
+     print(">> I will Test the UI")
+
+
 # Write a function to check if a number is divisible by five.
 # Return True if the number is divisible by 5. Otherwise, return False.
 def is_divisible_by_five(number):
