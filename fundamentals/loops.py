9ef352d1dee342e2c9ebbc6f0558f8e9c05ec5c2
Ankit Kumar <121275934+AnkitKumar5277@users.noreply.github.com>
2026-04-13 13:18:17 +0530

    Update loops.py

--- fundamentals/loops.py
+++ fundamentals/loops.py
@@ -1,3 +1,16 @@
+# Example 10: Floyd's Triangle
+# 1
+# 2 3
+# 4 5 6
+# 7 8 9 10
+rows = int(input("enter: ")
+number = 1
+for i in range(1, rows+1):
+  for j in range(1, i+1):
+    print(number, end= " ")
+    number += 1
+  print()
+
 # Example 5: Inverted half pyramid using numbers
 # 1 2 3 4 5
 # 1 2 3 4
