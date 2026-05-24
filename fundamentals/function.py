0e8f62e609dfcebbb9fea12793e34d7248d5895b
Ankit Kumar <121275934+AnkitKumar5277@users.noreply.github.com>
2026-05-24 22:39:13 +0530

    Update function.py

--- fundamentals/function.py
+++ fundamentals/function.py
@@ -37,23 +37,6 @@ pramod = make_pizza("tomato","olives", "\n")
 jayati = make_pizza("pineapple","olives","corn","paneer", "\n")
 vinay = make_pizza("tomato")
 
-def outer_function():
-    var1 = 30 # local
-
-    def inner_function():
-        var2 = 90
-        print(var1)
-
-    def inner_function2():
-        print(var1)
-
-    inner_function()
-    inner_function2()
-    # print(var2)
-
-outer_function()
-# inner_function()
-
 def add_before_ui_after_ui(func):
      def wrapper():
          print("Before the running UI TC")
