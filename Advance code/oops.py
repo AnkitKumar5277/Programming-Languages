5d031f7c1b846a5f60e67cc195d5e94845c2ed9b
Ankit Kumar <121275934+AnkitKumar5277@users.noreply.github.com>
2026-06-20 12:56:07 +0530

    Update oops.py

--- Advance code/oops.py
+++ Advance code/oops.py
@@ -1,102 +1,10 @@
-# Polymorphism 
-class Dog:
-    def sound(self):
-        print("Dog barks")
-class Cat:
-    def sound(self):
-        print("Cat meows")
-d = Dog()
-c = Cat()
-d.sound()
-c.sound()
 
-# class Attribute
-class Employee:
-  company = "Google"
-ankit = Employee() # class ka obj
-kumar = Employee() # object instantiation
-print(ankit.company) # ankit ki company
-print(kumar.company) # class attribute
-Employee.company = "Microsoft" # change class attribute
-print(ankit.company)
-print(kumar.company)
 
-#instance attribute & class attribute
-Class Sample 
-  name = "ankit"	  # class attribute
-obj = Sample() 	  # objects (instances
-obj.name = "vikky"	  # instance attribute / you cant change but you add instance attrubute 
-sample.name = "vikky" # class attribute / you can change class attrubute 
-print(sample.name) 
-print(obj.name) 
 
-# self
-class Employee:
-  company = "IBM"
-  def getSalary(self):
-    print(f"employee of {self.company} is {self.salary}")
-ankit = Employee()
-ankit.salary = 100000
-ankit.getSalary() # Employee.getSalary(ankit) #print the function components
 
-# static method
-class Person:
-  company = "Adobe"
-  @staticmethod  # mark decorate
-  def greet():
-    print("Good Morining, Sir")
-ankit = Person()
-Person.greet() #--> this is the statice method run code
-# or ankit.greet()
 
-# another example
-class Employee:
-  company = "META"
-  def getsalary(self, signature):
-    print(f"salary of emoplyee {self.company} is {self.salary}\n{signature}")
-  @staticmethod
-  def greet():
-    print("Good Morning, Sir")
-  
-  @staticmethod
-  def time():
-    print("10 am")
 
-ankit = Employee()
-ankit.salary = 100000
-ankit.getsalary("Thanks") # signature passed
-ankit.greet() # Employee.greet()
-ankit.time()
 
-# constructor
-class Programmer:
-  company = "Software X"
-  def __init__(self, name, salary, pin):
-      self.name = name
-      self.salary = salary
-      self.pin = pin
-p = Programmer("Ankit", 10000, 110033)
-print(p.name, p.salary, p.pin, p.company)
-r = Programmer("Shyam", 12000, 24001)
-print(r.name, r.salary, r.pin, r.company)
-
-# another example
-class Employee:
-    company = "Google"
-    def __init__(self, name, salary, subunit):
-        self.name = name
-        self.salary = salary
-        self.subunit = subunit
-        print("Employee is created!") 
-    
-    def getDetails(self):
-        print(f"The name of the employee is {self.name}")
-        print(f"The salary of the employee is {self.salary}")
-        print(f"The subunit of the employee is {self.subunit}")
-        
-harry = Employee("Harry", 100, "YouTube")
-# harry = Employee() --> This throws an error (missing 3 required positional arguments:)
-harry.getDetails()
 
 
 
