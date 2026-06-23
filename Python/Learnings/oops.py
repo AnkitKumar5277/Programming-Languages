2534bead2eb9d13f59cab49bfb0d6e4cea61cc15
Ankit Kumar <121275934+AnkitKumar5277@users.noreply.github.com>
2026-06-23 16:24:14 +0530

    Update oops.py

--- Python/Learnings/oops.py
+++ Python/Learnings/oops.py
@@ -1,12 +1,3 @@
-#oops example
-class Number:
-  def sum(self):
-    return self.a + self.b
-n = Number() # object instantiation
-n.a = 4
-n.b = 6 
-s = n.sum()
-print(s)
 
 # PascalCase
 # EmployeeName # used in class
@@ -18,28 +9,6 @@ print(s)
 '''
 class Employee:
 include methods & variables'''
-
-# object
-class RailwayForm:
-  formType = "RailwayForm"
-  def printData(self): # using self
-    print(f"Name is {self.name}")
-    print(f"Train is {self.train}")
-    
-ankitApp = RailwayForm() # class ka object
-ankitApp.name = "ankit"
-ankitApp.train = "Rajdhani Express"
-ankitApp.printData() # print
-
-# static method
-class Person:
-  company = "Adobe"
-  @staticmethod  # mark decorate
-  def greet():
-    print("Good Morining, Sir")
-ankit = Person()
-Person.greet() #--> this is the statice method run code
-# or ankit.greet()
 
 # another example
 class Employee:
@@ -60,32 +29,4 @@ ankit.getsalary("Thanks") # signature passed
 ankit.greet() # Employee.greet()
 ankit.time()
 
-# constructor
-class Programmer:
-  company = "Software X"
-  def __init__(self, name, salary, pin):
-      self.name = name
-      self.salary = salary
-      self.pin = pin
-p = Programmer("Ankit", 10000, 110033)
-print(p.name, p.salary, p.pin, p.company)
-r = Programmer("Shyam", 12000, 24001)
-print(r.name, r.salary, r.pin, r.company)
 
-# another example
-class Employee:
-    company = "Google"
-    def __init__(self, name, salary, subunit):
-        self.name = name
-        self.salary = salary
-        self.subunit = subunit
-        print("Employee is created!") 
-    
-    def getDetails(self):
-        print(f"The name of the employee is {self.name}")
-        print(f"The salary of the employee is {self.salary}")
-        print(f"The subunit of the employee is {self.subunit}")
-        
-harry = Employee("Harry", 100, "YouTube")
-# harry = Employee() --> This throws an error (missing 3 required positional arguments:)
-harry.getDetails()
