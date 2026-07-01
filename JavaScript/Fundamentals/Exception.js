03f72cd2a3dc0d9a576554722a5a572c4f2a8e0a
Ankit Kumar <121275934+AnkitKumar5277@users.noreply.github.com>
2026-07-01 22:08:19 +0530

    Update Exception.js

--- JavaScript/Fundamentals/Exception.js
+++ JavaScript/Fundamentals/Exception.js
@@ -4,3 +4,16 @@ try {
 } catch (err) {
     console.log("Error:", err);
 }
+
+// Try Catch
+let age = 15;
+try {
+    if (age < 18) {
+        throw new Error("You are not eligible to vote.");
+    }
+    console.log("You can vote.");
+} catch (error) {
+    console.log("Error:", error.message);
+}
+// ERROR!
+// Error: You are not eligible to vote.
