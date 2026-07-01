0a5ba5117042d97b99d3362ebbfa56137c73aded
Ankit Kumar <121275934+AnkitKumar5277@users.noreply.github.com>
2026-07-01 20:33:46 +0530

    Create Array.js

---
+++ JavaScript/Fundamentals/Array.js
@@ -0,0 +1,12 @@
+// Arrays
+let users = ["A", "B", "C"];
+console.log(users.push("D"));     // add
+users.pop();         // remove
+console.log(users.length);        // size
+console.log(users.includes("A")); // check
+console.log(users);
+// 4
+// D
+// 3
+// true
+// [ 'A', 'B', 'C' ]
