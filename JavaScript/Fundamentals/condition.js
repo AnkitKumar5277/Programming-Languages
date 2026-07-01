05fc7946309eec30bd4c0836ac4ccbcbeb10dd7e
Ankit Kumar <121275934+AnkitKumar5277@users.noreply.github.com>
2026-07-01 19:49:55 +0530

    Update condition.js

--- JavaScript/Fundamentals/condition.js
+++ JavaScript/Fundamentals/condition.js
@@ -1,8 +1,23 @@
+// if else
 let marks = 80;
 if (marks >= 40) {
   console.log("Pass");
 } else {
   console.log("Fail");
 }
-
 // Pass
+
+// Switch
+let day = Number(prompt("Enter day number : "));
+switch (day) {
+    case 1:
+        console.log("Sunday");
+        break;
+    case 2:
+        console.log("Monday");
+        break;
+    default:
+        console.log("Invalid day");
+}
+// Enter day number : 4
+// Invalid day
