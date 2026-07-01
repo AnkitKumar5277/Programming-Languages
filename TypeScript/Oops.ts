63b844f9cb0048707924e64b70df274529215e0b
Ankit Kumar <121275934+AnkitKumar5277@users.noreply.github.com>
2026-07-01 22:19:07 +0530

    Create Oops.ts

---
+++ TypeScript/Oops.ts
@@ -0,0 +1,9 @@
+// Interface - Can be used to make the parameters cleaner and reusable
+interface User {
+ name: string;
+ email: string;
+}
+const user: User = {
+ name: "Ankit",
+ email: "ankit@test.com"
+};
