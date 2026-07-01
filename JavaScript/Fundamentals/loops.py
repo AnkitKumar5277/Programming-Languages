f900eed5fb565d90e1d5594d9f4cf0b220e29ca1
Ankit Kumar <121275934+AnkitKumar5277@users.noreply.github.com>
2026-07-01 20:14:13 +0530

    Create loops.py

---
+++ JavaScript/Fundamentals/loops.py
@@ -0,0 +1,54 @@
+// Loops
+for (let i = 0; i < 5; i++) {
+ console.log(i);
+}
+// 0
+// 1
+// 2
+// 3
+// 4
+
+// for...of (Modern)
+const items = ['a', 'b', 'c'];
+for (const item of items) {
+console.log(item);
+} 
+// a
+// b
+// c
+
+// forEach
+let numbers = [1, 2, 3];
+numbers.forEach((num) => {
+    console.log(num);
+});
+// 1
+// 2
+// 3
+
+// While Loop
+let i = 0;
+while (i < 5) {
+console.log(i);
+i++;
+} 
+// 0
+// 1
+// 2
+// 3
+// 4
+
+// Do While Loop
+let j = 0;
+do {
+console.log(j);
+j++;
+} while (j < 5); 
+// 0
+// 1
+// 2
+// 3
+// 4
+
+
+
