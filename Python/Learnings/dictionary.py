2d793750e1952be5008d51ab07d75947a2823da5
Ankit Kumar <121275934+AnkitKumar5277@users.noreply.github.com>
2026-07-06 22:55:15 +0530

    Create dictionary.py

---
+++ Python/Learnings/dictionary.py
@@ -0,0 +1,43 @@
+# Write a function to merge two dictionaries.
+def merge_dictionaries(dict1, dict2):
+    merged_dict = dict1.copy()
+    merged_dict.update(dict2)
+    return merged_dict
+dict1 = {'a': 1, 'b': 2}
+dict2 = {'c': 3, 'd': 4}
+merged = merge_dictionaries(dict1, dict2)
+print(merged)  # Output: {'a': 1, 'b': 2, 'c': 3, 'd': 4}
+
+# Example 2: Using the ** Operator
+d1 = {1: 'a', 2: 'b'}
+d2 = {2: 'c', 4: 'd'}
+print({**d1, **d2})
+# {1: 'a', 2: 'c', 4: 'd'}
+
+# Example 3: Using copy() and update()
+d1 = {1: 'a', 2: 'b'}
+d2 = {2: 'c', 4: 'd'}
+d3 = d2.copy()
+d3.update(d1)
+print(d3)
+# {2: 'b', 4: 'd', 1: 'a'}
+
+# Create a dictionary
+my_dict = {"name": "Amit", "age": 20}
+my_dict["city"] = "Delhi"  # New element added
+print(my_dict)  # Output: {'name': 'Amit', 'age': 20, 'city': 'Delhi'}
+my_dict["age"] = 21  # Age overwritten
+print(my_dict)  # Output: {'name': 'Amit', 'age': 21, 'city': 'Delhi'}
+my_dict.update({"job": "Student", "hobby": "Reading"})
+print(my_dict)  # Output: {'name': 'Amit', 'age': 21, 'city': 'Delhi', 'job': 'Student', 'hobby': 'Reading'}
+
+fruits = ["apple", "banana", "orange"]
+print("apple" in fruits)  # Output: True
+print("grape" in fruits)  # Output: False
+
+# Python Program to Merge Two Dictionaries
+d1 = {1:'a', 2:'b'}
+d2 = {2:'c', 4:'d'}
+print(d1|d2)
+# {1: 'a', 2: 'c', 4: 'd'}
+
