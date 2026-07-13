8eec60e0f5931a21e8dd817cd7df25b817e7f0d9
Ankit Kumar <121275934+AnkitKumar5277@users.noreply.github.com>
2026-07-13 22:44:49 +0530

    Create hoisting.js

---
+++ JavaScript/Important/hoisting.js
@@ -0,0 +1,12 @@
+console.log(a)
+var a = 10;
+console.log(a)
+// undefined
+// 10
+
+var a;            // Hoisted
+console.log(a);   // undefined
+a = 10;
+console.log(a);   // 10
+// undefined
+// 10
