bb9adff5c8c5eaafde557c6cfaca7726ffed2215
Ankit Kumar <121275934+AnkitKumar5277@users.noreply.github.com>
2026-07-14 20:38:06 +0530

    Create Callback Function.js

---
+++ JavaScript/Important/Callback Function.js
@@ -0,0 +1,12 @@
+// Callback Function
+function greet(name, callback) {
+  console.log("Hello", name);
+  callback();
+}
+function sayBye() {
+  console.log("Goodbye!");
+}
+greet("Ankit", sayBye);
+// Hello Ankit
+// Goodbye!
+
