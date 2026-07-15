388c6d995416f41b23e831c2b0f2edb9fb0159ea
Ankit Kumar <121275934+AnkitKumar5277@users.noreply.github.com>
2026-07-15 16:30:02 +0530

    Create Dictionary & Sets.py

---
+++ Python/Interview/Dictionary & Sets.py
@@ -0,0 +1,11 @@
+# *dictionary 
+a = {"key":"value", 
+       "key1":"value2"} 
+print(a["key"])
+# value
+
+# *set 
+my_set = {1, 2, 3, 4}  
+my_set = set([1, 2, 3, 4])  
+my_set = set()  # empty set
+# {1, 2, 3, 4}
