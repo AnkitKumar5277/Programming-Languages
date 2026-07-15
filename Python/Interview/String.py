35676a80e2b902ba70e073c7be8c34b4f1d141e8
Ankit Kumar <121275934+AnkitKumar5277@users.noreply.github.com>
2026-07-15 22:18:50 +0530

    Create String.py

---
+++ Python/Interview/String.py
@@ -0,0 +1,14 @@
+# concatenate two strings:
+string1 = "Hello"
+string2 = "world"
+result = string1 + " " + string2
+print(result)
+# Hello world
+
+
+# .join() -> concatenate
+a = "ankit"
+b = "kumar"
+result = " ".join([a, b])
+print(result)
+# ankitkumar
