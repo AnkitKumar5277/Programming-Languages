0329828e3b9e75bfc797959de172f4e913a9895f
Ankit Kumar <121275934+AnkitKumar5277@users.noreply.github.com>
2026-07-15 22:40:03 +0530

    Update Loop.py

--- Python/Interview/Loop.py
+++ Python/Interview/Loop.py
@@ -18,3 +18,36 @@ for num in l:
 # 3
 # 4
 # 5
+
+# break
+for i in range(1, 6):
+    if i == 3:
+        break
+    print(i)
+# 1
+# 2
+
+# continue
+for i in range(1, 6):
+    if i ==3:
+        continue
+    print(i)
+# 1
+# 2
+# 4
+# 5
+
+# pass
+for i in range(1, 6):
+    if i == 3:
+        pass
+    print(i)
+# 1
+# 2
+# 3
+# 4
+# 5
+
+
+    
+    
