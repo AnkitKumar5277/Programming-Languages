a7d1af329c1823e9f3673a482db68c7e35eabeef
Ankit Kumar <121275934+AnkitKumar5277@users.noreply.github.com>
2026-07-17 22:47:45 +0530

    Update Destructuring Assignment.js

--- JavaScript/Important/Destructuring Assignment.js
+++ JavaScript/Important/Destructuring Assignment.js
@@ -8,5 +8,17 @@ const { name, age} = user;
 console.log(name); // Ankit
 console.log(age); // 22
 
+// Array Destructuring
+// Normal Way
+let colors = ["Red", "Green", "Blue"];
+let first = colors[0];
+let second = colors[1];
+console.log(first);   // Red
+console.log(second);  // Green
 
+// Using Destructuring
+let colors = ["Red", "Green", "Blue"];
+let [first, second] = colors;
+console.log(first);   // Red
+console.log(second);  // Green
 
