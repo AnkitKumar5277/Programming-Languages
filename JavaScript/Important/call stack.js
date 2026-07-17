db8f38ebbced71bf21a72ec85a1c40b2a870051a
Ankit Kumar <121275934+AnkitKumar5277@users.noreply.github.com>
2026-07-17 13:40:00 +0530

    Create call stack.js

---
+++ JavaScript/Important/call stack.js
@@ -0,0 +1,15 @@
+def greet():
+    print("Hello")
+
+def welcome():
+    greet()
+    print("Welcome")
+
+def main():
+    welcome()
+    print("Main End")
+
+main()
+// Hello
+// Welcome
+// Main End
