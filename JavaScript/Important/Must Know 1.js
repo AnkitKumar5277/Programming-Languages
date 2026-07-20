1458ea0e77869b0408a05045226d44c4c4ea9444
Ankit Kumar <121275934+AnkitKumar5277@users.noreply.github.com>
2026-07-20 23:46:46 +0530

    Update Must Know 1.js

--- JavaScript/Important/Must Know 1.js
+++ JavaScript/Important/Must Know 1.js
@@ -51,3 +51,39 @@ a = 10;
 console.log(a);   // 10
 // undefined
 // 10
+
+
+// Global Scope
+let name = "ankit"; // Global variable
+function showname() {
+  console.log(name) 
+}
+showname();
+console.log(name);
+// ankit
+// ankit
+
+
+// Function Scope
+function demo() {
+    var age = 22;
+    console.log(age);
+}
+demo();
+// console.log(age); -> Error
+// 22
+
+
+// Block Scope
+if (true) {
+    let city = "Delhi";
+    const country = "India";
+    console.log(city);
+    console.log(country);
+}
+// Delhi
+// India
+// console.log(city);      // Error
+// console.log(country);   // Error
+
+
