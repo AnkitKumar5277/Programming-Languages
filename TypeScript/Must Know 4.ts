7d82fabbd0e05b7bcc9086841d3b1b0bfbb5fdf9
Ankit Kumar <121275934+AnkitKumar5277@users.noreply.github.com>
2026-07-20 23:12:39 +0530

    Update Must Know 4.ts

--- TypeScript/Must Know 4.ts
+++ TypeScript/Must Know 4.ts
@@ -25,6 +25,48 @@ function greet(name: string, city?: string) {
   console.log(name);
 }
 
+// void
+function greet(): void{
+  console.log("hello");
+}
+console.log(greet());
+// hello
+// undefined
 
+// Intersection type
+type A = {  name: string;  };
+type B = {  age: number;  };
+type Person = A & B;
+const person: Person = {
+  name: "Ankit",
+  age: 22
+};
+console.log(person);
 
+// Nullish Coalescing (??)
+let userName = null;
+let name = userName ?? "Guest";
+console.log(name);
+// Guest
 
+// Optional Chaining (?.)?
+const user = {
+  name: "Ankit",
+  address: {
+    city: "Delhi"
+  }
+};
+console.log(user?.address?.city);
+// Delhi
+
+// never
+function throwError(): never {
+  throw new Error("Something went wrong!");
+}
+
+// Literal Type
+let direction: "left" | "right";
+direction = "left";   // ✅ Valid
+direction = "right";  // ✅ Valid
+console.log(direction);
+// right
