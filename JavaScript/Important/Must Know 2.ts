ba528029fa7965efaa7d9cf6a2a92db86ffd0658
Ankit Kumar <121275934+AnkitKumar5277@users.noreply.github.com>
2026-07-21 22:55:05 +0530

    Update Must Know 2.ts

--- JavaScript/Important/Must Know 2.ts
+++ JavaScript/Important/Must Know 2.ts
@@ -47,3 +47,42 @@ async function fetchData() {
   console.log(result);
 }
 fetchData();
+
+
+// Event Loop
+// setTimeout()
+console.log("Start");
+setTimeout(() => {
+    console.log("Hello");
+}, 2000);
+console.log("End");
+// Start
+// End
+// Hello - prints after 2 seconds
+
+// Promise (Microtask Queue)
+
+// Example 2: Promise (Microtask Queue)
+console.log("Start");
+Promise.resolve().then(() => {
+    console.log("Promise");
+});
+console.log("End");
+// Start
+// End
+// Promise
+
+// Example 3: Promise vs setTimeout
+console.log("Start");
+setTimeout(() => {
+    console.log("Timeout");
+}, 0);
+Promise.resolve().then(() => {
+    console.log("Promise");
+});
+console.log("End");
+// Start
+// End
+// Promise
+// Timeout
+
