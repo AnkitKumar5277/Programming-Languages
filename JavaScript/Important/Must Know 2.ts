56f6525bf5ecfdd3f8943226be9b6527a28634f3
Ankit Kumar <121275934+AnkitKumar5277@users.noreply.github.com>
2026-07-21 22:42:39 +0530

    Update Must Know 2.ts

--- JavaScript/Important/Must Know 2.ts
+++ JavaScript/Important/Must Know 2.ts
@@ -31,3 +31,19 @@ promise
 
 // if paymentSuccess = false):
 // Payment Failed
+
+// Async Await
+// .then() aur .catch() ki jagah normal synchronous jaisa code likh sakte hain.
+function getData() {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve("Data received!");
+    }, 2000);
+  });
+}
+async function fetchData() {
+  console.log("Fetching data...");
+  let result = await getData();
+  console.log(result);
+}
+fetchData();
