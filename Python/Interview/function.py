fca2c5b6c3c2bbb41e202631c6eb2cd781fd29de
Ankit Kumar <121275934+AnkitKumar5277@users.noreply.github.com>
2026-07-23 00:49:56 +0530

    Update function.py

--- Python/Interview/function.py
+++ Python/Interview/function.py
@@ -28,3 +28,42 @@ print(next(gen))   # 1
 print(next(gen))   # 2
 print(next(gen))   # 3
 
+# iterator
+numbers = [10, 20, 30]
+it = iter(numbers)
+print(next(it))   # 10
+print(next(it))   # 20
+print(next(it))   # 30
+
+# Positional Arguments
+def greet(name, age):
+    print(name, age)
+greet("Ankit", 22)
+# Ankit 22
+
+# Keyword Arguments
+def greet(name, age):
+    print(name, age)
+greet(age=22, name="Ankit")
+
+# Default Arguments
+def greet(name="Guest"):
+    print("Hello", name)
+greet()
+greet("Ankit")
+# Hello Guest
+# Hello Ankit
+
+# Variable Length Arguments (*args)
+def add(*args):
+    print(sum(args))
+add(10, 20)
+add(10, 20, 30, 40)
+
+# Variable Length Keyword Arguments (**kwargs)
+def student(**kwargs):
+    print(kwargs)
+student(name="Ankit", age=22, city="Delhi")
+{'name': 'Ankit', 'age': 22, 'city': 'Delhi'}
+
+
