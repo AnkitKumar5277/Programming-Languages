cce12e4d381a71532f8db377e6f23ea77ae44567
Ankit Kumar <121275934+AnkitKumar5277@users.noreply.github.com>
2026-07-24 13:28:35 +0530

    Update Function Delcaration vs Expression.js

--- JavaScript/Important/Function Delcaration vs Expression.js
+++ JavaScript/Important/Function Delcaration vs Expression.js
@@ -1,15 +1,1 @@
-// Difference between Function Declaration and Function Expression?
 
-// Function Declaration
-function greet() {
-  console.log("hello");
-}
-greet();
-// Hello
-
-// Function Expression
-const greet = function() {
-  console.log("Hello");
-}
-greet();
-// Hello
