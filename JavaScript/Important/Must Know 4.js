5a7090603eb46e5848907bc175025ab0c3febf01
Ankit Kumar <121275934+AnkitKumar5277@users.noreply.github.com>
2026-07-24 13:22:47 +0530

    Update Must Know 4.js

--- JavaScript/Important/Must Know 4.js
+++ JavaScript/Important/Must Know 4.js
@@ -1,6 +1,10 @@
 let name = "Ankit";
 let age = 22;
-
 let message = `My name is ${name} and I am ${age} years old.`;
-
 console.log(message);
+
+
+// nan
+console.log(0 / 0);        // NaN
+console.log(Number("abc")); // NaN
+console.log(Math.sqrt(-1)); // NaN
