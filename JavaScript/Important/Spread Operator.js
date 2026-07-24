c35e5dbb0d992d8864bff63a59aef3991aff0be7
Ankit Kumar <121275934+AnkitKumar5277@users.noreply.github.com>
2026-07-24 10:29:17 +0530

    Update Spread Operator.js

--- JavaScript/Important/Spread Operator.js
+++ JavaScript/Important/Spread Operator.js
@@ -7,4 +7,27 @@ console.log(a);
 let a = 10;
 // Output : ReferenceError
 
+// Object Destructuring
+// Destructuring
+const user = {
+  name: "Ankit", 
+  age: 22
+};
+const { name, age} = user;
+console.log(name); // Ankit
+console.log(age); // 22
+
+// Array Destructuring
+// Normal Way
+let colors = ["Red", "Green", "Blue"];
+let first = colors[0];
+let second = colors[1];
+console.log(first);   // Red
+console.log(second);  // Green
+
+// Using Destructuring
+let colors = ["Red", "Green", "Blue"];
+let [first, second] = colors;
+console.log(first);   // Red
+console.log(second);  // Green
 
