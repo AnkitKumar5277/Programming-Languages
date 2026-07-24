f7f03e38ac5e89680ad22b2ad183c9588bd53c6f
Ankit Kumar <121275934+AnkitKumar5277@users.noreply.github.com>
2026-07-24 13:20:43 +0530

    Update Must Know 3.js

--- JavaScript/Important/Must Know 3.js
+++ JavaScript/Important/Must Know 3.js
@@ -63,3 +63,23 @@ console.log(allEven);
 </body>
 </html>
 
+
+// This Keyword and Arrow Function
+// Arrow Function 
+const add = (a, b) => a + b;
+console.log(add(5,3));
+// 8
+
+const person = {
+  name: "Ankit",
+  normalFunction: function () {
+    console.log("Normal:", this.name);
+  },
+  arrowFunction: () => {
+    console.log("Arrow:", this.name);
+  }
+};
+person.normalFunction();
+person.arrowFunction();
+// Normal: Ankit
+// Arrow: undefined
