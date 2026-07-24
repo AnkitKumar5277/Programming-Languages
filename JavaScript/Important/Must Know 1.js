92b11603412e777d892efbaaf5468cd5f7b34b72
Ankit Kumar <121275934+AnkitKumar5277@users.noreply.github.com>
2026-07-24 13:29:13 +0530

    Update Must Know 1.js

--- JavaScript/Important/Must Know 1.js
+++ JavaScript/Important/Must Know 1.js
@@ -1,3 +1,19 @@
+// Difference between Function Declaration and Function Expression?
+// Function Declaration
+function greet() {
+  console.log("hello");
+}
+greet();
+// Hello
+// Function Expression
+const greet = function() {
+  console.log("Hello");
+}
+greet();
+// Hello
+
+
+
 // var, let, and const
 // var
 var a = 10;
