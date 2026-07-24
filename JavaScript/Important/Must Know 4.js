f35a8faa63cf5aa116fa29e9f3b746d660d540fb
Ankit Kumar <121275934+AnkitKumar5277@users.noreply.github.com>
2026-07-24 13:23:39 +0530

    Update Must Know 4.js

--- JavaScript/Important/Must Know 4.js
+++ JavaScript/Important/Must Know 4.js
@@ -8,3 +8,23 @@ console.log(message);
 console.log(0 / 0);        // NaN
 console.log(Number("abc")); // NaN
 console.log(Math.sqrt(-1)); // NaN
+
+
+
+// slice() Example (Original array safe)
+let fruits = ["Apple", "Banana", "Mango", "Orange"];
+let result = fruits.slice(1, 3);
+console.log(result);
+console.log(fruits);
+// ["Banana", "Mango"]
+// ["Apple", "Banana", "Mango", "Orange"]
+
+// 2. splice() Example (Remove Elements)
+let fruits = ["Apple", "Banana", "Mango", "Orange"];
+let removed = fruits.splice(1, 2);
+console.log(removed);
+console.log(fruits);
+// Output
+// ["Banana", "Mango"]
+// ["Apple", "Orange"]
+
