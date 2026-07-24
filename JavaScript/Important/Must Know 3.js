340fef8cf7f3dee94d0fd8a5accf3797b75749ca
Ankit Kumar <121275934+AnkitKumar5277@users.noreply.github.com>
2026-07-24 13:21:28 +0530

    Update Must Know 3.js

--- JavaScript/Important/Must Know 3.js
+++ JavaScript/Important/Must Know 3.js
@@ -83,3 +83,38 @@ person.normalFunction();
 person.arrowFunction();
 // Normal: Ankit
 // Arrow: undefined
+
+
+const arr = [1, 2];
+const newArr = [...arr, 3];
+console.log(newArr);
+
+// Temporal Dead Zone (TDZ) 
+console.log(a);
+let a = 10;
+// Output : ReferenceError
+
+// Object Destructuring
+// Destructuring
+const user = {
+  name: "Ankit", 
+  age: 22
+};
+const { name, age} = user;
+console.log(name); // Ankit
+console.log(age); // 22
+
+// Array Destructuring
+// Normal Way
+let colors = ["Red", "Green", "Blue"];
+let first = colors[0];
+let second = colors[1];
+console.log(first);   // Red
+console.log(second);  // Green
+
+// Using Destructuring
+let colors = ["Red", "Green", "Blue"];
+let [first, second] = colors;
+console.log(first);   // Red
+console.log(second);  // Green
+
