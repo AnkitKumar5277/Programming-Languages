6330ea7251582cb092a1a3e7fc9e585de9d5e418
Ankit Kumar <121275934+AnkitKumar5277@users.noreply.github.com>
2026-07-24 13:08:55 +0530

    Update slice() vs splice().js

--- JavaScript/Important/slice() vs splice().js
+++ JavaScript/Important/slice() vs splice().js
@@ -4,8 +4,8 @@ let fruits = ["Apple", "Banana", "Mango", "Orange"];
 let result = fruits.slice(1, 3);
 console.log(result);
 console.log(fruits);
-["Banana", "Mango"]
-["Apple", "Banana", "Mango", "Orange"]
+// ["Banana", "Mango"]
+// ["Apple", "Banana", "Mango", "Orange"]
 
 // 2. splice() Example (Remove Elements)
 let fruits = ["Apple", "Banana", "Mango", "Orange"];
@@ -14,5 +14,4 @@ console.log(removed);
 console.log(fruits);
 // Output
 // ["Banana", "Mango"]
-
 // ["Apple", "Orange"]
