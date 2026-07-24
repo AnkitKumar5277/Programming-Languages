bbc9405ba60d8cf255de5e018aaea5c119b811d1
Ankit Kumar <121275934+AnkitKumar5277@users.noreply.github.com>
2026-07-24 17:48:20 +0530

    Update Must Know 4.js

--- JavaScript/Important/Must Know 4.js
+++ JavaScript/Important/Must Know 4.js
@@ -72,3 +72,21 @@ console.log(fruits);
 // ["Banana", "Mango"]
 // ["Apple", "Orange"]
 
+
+// Array
+let fruits = ["Apple", "Banana", "Mango"];
+console.log(fruits);
+console.log(fruits[0]); // Apple
+console.log(fruits[1]); // Banana
+console.log(fruits[2]); // Mango
+
+// objects
+let student = {
+    name: "Ankit",
+    age: 22,
+    city: "Delhi"
+};
+console.log(student);
+console.log(student.name);
+console.log(student.age);
+console.log(student.city);
